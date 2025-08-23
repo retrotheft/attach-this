d323bf7ef076dc30df998f8cd919e4a9d3acd514
Jim Bridger <retrotheft@gmail.com>
2025-08-23 10:30:38 +1000

    removed old components

--- src/lib/stores/replace.ts
+++ src/lib/stores/replace.ts
@@ -1,8 +1,5 @@
-import ReplaceTextNodes from "$lib/components/ReplaceTextNodes.svelte";
-import ReplaceLetters from "$lib/components/ReplaceLetters.svelte";
 import { writable } from 'svelte/store';
 import { type Component } from "svelte"
-import ReplaceWords from "$lib/components/ReplaceWords.svelte";
 import ReplaceAny from "$lib/components/ReplaceAny.svelte";
 
 export function withProps(Component: Component<any>, defaultProps: Record<string, any>) {
@@ -16,23 +13,9 @@ export function createReplaceStore(lookupTable: Record<string, Record<string, st
    const { subscribe, set, update } = writable('en');
 
    return {
-      // Store methods
       subscribe,
       set,
       update,
-
-      // Component getter with injected props - now reactive!
-      // get text() {
-      //    return withProps(ReplaceTextNodes, { lookupTable, locale: { subscribe } });
-      // },
-
-      // get words() {
-      //    return withProps(ReplaceWords, { lookupTable, locale: { subscribe } });
-      // },
-
-      // get letters() {
-      //    return withProps(ReplaceLetters, { lookupTable, locale: { subscribe } })
-      // }
 
       get text() {
          return withProps(ReplaceAny, { lookupTable, locale: { subscribe }, strategy: 'textNodes' });
